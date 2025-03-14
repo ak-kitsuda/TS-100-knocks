e82d05d7ed77177ab8b6329ddf29c149d4cbfd1d
ak-kitsuda <ak-kitsuda@creationline.com>
2025-03-14 18:09:33 +0900

    init

---
+++ src/pages/AnswerPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <p>工事中</p>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  name: 'AnswerPage'
+})
+</script>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,10 +1,15 @@
+import AnswerPage from 'src/pages/AnswerPage.vue';
+import KnockPage from 'src/pages/KnockPage.vue';
 import type { RouteRecordRaw } from 'vue-router';
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [
+      { path: '', component: KnockPage },
+      { path: 'answer', component: AnswerPage }
+    ],
   },
 
   // Always leave this as last one,
