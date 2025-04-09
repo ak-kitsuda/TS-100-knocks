75405572488e1231c95caf5a90f4c97e349b3361
ak-kitsuda <ak-kitsuda@creationline.com>
2025-04-09 17:02:36 +0900

    KnockPageのstoreの値を分割代入するようにした

--- src/pages/KnockPage.vue
+++ src/pages/KnockPage.vue
@@ -1,33 +1,29 @@
 <template>
   <q-page class="flex items-center column q-pa-md">
-    <div v-if="currentStateStore.currentQuiz" class="full-width q-mt-md" style="max-width: 600px;">
+    <div v-if="currentQuiz" class="full-width q-mt-md" style="max-width: 600px;">
       <!-- 解説ボタン -->
-      <q-btn class="absolute-top-right q-ma-sm" label="解説" type="a" :href="currentStateStore.currentQuiz.link"
-        target="_blank" color="primary" size="lg" />
+      <q-btn class="absolute-top-right q-ma-sm" label="解説" type="a" :href="currentQuiz.link" target="_blank"
+        color="primary" size="lg" />
 
-      <!-- 進捗表示・回答結果・正解数を横並び -->
+      <!-- 進捗表示 -->
       <div class="row justify-around items-center q-pa-sm">
-
-        <!-- 進捗表示 -->
         <div class="col text-h6 text-center">
-          <p>{{ currentStateStore.progress }} / {{ QUIZ_NUM }}</p>
+          <p>{{ progress }} / {{ QUIZ_NUM }}</p>
         </div>
       </div>
 
       <!-- 問題文 -->
       <q-card class="q-mt-sm q-pa-md flex column items-center">
-        <div v-if="currentStateStore.currentQuiz.questionCode"
-          class="full-width bg-black text-white q-px-sm rounded-borders">
-          <div v-for="(code, key) in currentStateStore.currentQuiz.questionCode" :key="key">
+        <div v-if="currentQuiz.questionCode" class="full-width bg-black text-white q-px-sm rounded-borders">
+          <div v-for="(code, key) in currentQuiz.questionCode" :key="key">
             <pre class="text-mono">{{ code }}</pre>
           </div>
         </div>
-        <p class="text-h6 text-left full-width q-mt-sm">{{ currentStateStore.currentQuiz.questionText }}</p>
+        <p class="text-h6 text-left full-width q-mt-sm">{{ currentQuiz.questionText }}</p>
       </q-card>
 
       <!-- 選択肢ボタン -->
-      <div v-for="(value, key) in currentStateStore.currentQuiz.options" :key="key"
-        class="q-mt-sm flex column items-center full-width">
+      <div v-for="(value, key) in currentQuiz.options" :key="key" class="q-mt-sm flex column items-center full-width">
         <q-btn :label="`${key} : ${value}`" @click="handleClickOption(key)" style="text-transform: none;"
           class="full-width" color="primary" size="lg" />
       </div>
@@ -35,28 +31,27 @@
       <div class="flex column justify-center">
         <!-- 回答後の表示エリア（未回答時は空白） -->
         <div class="q-mt-sm row items-center" style="min-height: 60px;"
-          :style="{ visibility: currentStateStore.isAnswered ? 'visible' : 'hidden' }">
+          :style="{ visibility: isAnswered ? 'visible' : 'hidden' }">
 
           <!-- 回答部分 -->
           <div class="col-6 text-h5 text-bold text-center">
             <pre
-              :class="isCorrect ? 'text-blue' : 'text-red'">{{ isCorrect ? "正解！" : "不正解" }}（ 答え：{{ currentStateStore.currentQuiz.answer }} ）</pre>
+              :class="isCorrect ? 'text-blue' : 'text-red'">{{ isCorrect ? "正解！" : "不正解" }}（ 答え：{{ currentQuiz.answer }} ）</pre>
           </div>
 
           <!-- ボタン部分 -->
           <div class="col-6 flex justify-center">
-            <q-btn v-if="currentStateStore.progress === QUIZ_NUM" label="Reset" @click="handleClickReset" color="orange"
-              size="lg" class="full-width" />
+            <q-btn v-if="progress === QUIZ_NUM" label="Reset" @click="handleClickReset" color="orange" size="lg"
+              class="full-width" />
             <q-btn v-else label="Next" @click="handleClickNext" color="secondary" size="lg" class="full-width" />
           </div>
-
         </div>
       </div>
 
       <!-- 正解数（最後の問題時のみ表示） -->
       <div class="q-mt-md col text-h3 text-bold text-center ">
-        <p :style="{ visibility: currentStateStore.progress === QUIZ_NUM ? 'visible' : 'hidden' }">
-          正解率：{{ currentStateStore.corrections }} %
+        <p :style="{ visibility: progress === QUIZ_NUM ? 'visible' : 'hidden' }">
+          正解率：{{ corrections }} %
         </p>
       </div>
     </div>
@@ -69,34 +64,36 @@
 
 <script setup lang="ts">
 import { useCurrentStateStore } from 'src/stores/CurrentStateStore';
+import { storeToRefs } from 'pinia';
 import { ref } from 'vue';
 
 const QUIZ_NUM: number = 100;
 
 const currentStateStore = useCurrentStateStore();
+const { currentQuiz, progress, isAnswered, corrections } = storeToRefs(currentStateStore);
+
 type ansOption = 'a' | 'b' | 'c' | 'd' | null;
 const chosenAns = ref<ansOption>(null);
 const isCorrect = ref<boolean>(false);
 
-
 const handleClickOption = (ans: Exclude<ansOption, null>): void => {
-  if (currentStateStore.currentQuiz && !currentStateStore.isAnswered) {
-    currentStateStore.isAnswered = true;
+  if (currentQuiz.value && !isAnswered.value) {
+    isAnswered.value = true;
     chosenAns.value = ans;
-    if (ans === currentStateStore.currentQuiz.answer) {
+    if (ans === currentQuiz.value.answer) {
       isCorrect.value = true;
     }
-    currentStateStore.progress++;
+    progress.value++;
   }
-  currentStateStore.judge(isCorrect.value)
-}
+  currentStateStore.judge(isCorrect.value);
+};
 
 const handleClickNext = (): void => {
   currentStateStore.setCurrentQuiz();
-  currentStateStore.isAnswered = false;
+  isAnswered.value = false;
   isCorrect.value = false;
   chosenAns.value = null;
-}
+};
 
 const handleClickReset = (): void => {
   currentStateStore.reset();
@@ -105,10 +102,6 @@ const handleClickReset = (): void => {
 };
 
 defineOptions({
-  name: 'KnockPage'
-})
-
-
-
-
+  name: 'KnockPage',
+});
 </script>
